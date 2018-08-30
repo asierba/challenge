99c2d813317254e0c54f70cba2845773432acea2
Asier <asierba@gmail.com>
2018-08-30 15:08:34 +0100

    cleanup

--- Algorithms.CountingBits/PositiveBitCounter.cs
+++ Algorithms.CountingBits/PositiveBitCounter.cs
@@ -18,30 +18,29 @@ namespace Payvision.CodeChallenge.Algorithms.CountingBits
             if (input < 0)
                 throw new ArgumentException(nameof(input));
 
-            var bits = ToBitsArray(input).ToList();
+            var bits = ToBitArray(input).ToList();
 
             var numOfOnes = bits.Count(x => x);
-
             var indexOfOnes = GetIndexesForOnes(bits);
             
             return new List<int>{numOfOnes}.Concat(indexOfOnes);
         }
 
-        private IEnumerable<int> GetIndexesForOnes(IReadOnlyList<bool> bits)
+        private static IEnumerable<bool> ToBitArray(int number)
+        {
+            var b = new BitArray(new[] {number});
+            var bits = new bool[b.Count];
+            b.CopyTo(bits, 0);
+            return bits;
+        }
+
+        private static IEnumerable<int> GetIndexesForOnes(IReadOnlyList<bool> bits)
         {
             for (var i = 0; i < bits.Count(); i++)
             {
                 if(bits[i])
                     yield return i;
             }
-        }
-
-        private static IEnumerable<bool> ToBitsArray(int number)
-        {
-            var b = new BitArray(new[] {number});
-            var bits = new bool[b.Count];
-            b.CopyTo(bits, 0);
-            return bits;
         }
     }
 }
