75964ae285e4bf07bffed222019a9668dfddb0c0
Asier <asierba@gmail.com>
2018-08-30 09:27:40 +0100

    Make PositiveBitCounter work in a dirty way

--- Algorithms.CountingBits/PositiveBitCounter.cs
+++ Algorithms.CountingBits/PositiveBitCounter.cs
@@ -4,6 +4,9 @@
 // </copyright>
 // -----------------------------------------------------------------------
 
+using System.Collections;
+using System.Linq;
+
 namespace Payvision.CodeChallenge.Algorithms.CountingBits
 {
     using System;
@@ -13,7 +16,33 @@ namespace Payvision.CodeChallenge.Algorithms.CountingBits
     {
         public IEnumerable<int> Count(int input)
         {
-            throw new NotImplementedException();
+            if (input < 0)
+                throw new ArgumentException(nameof(input));
+
+            var bits = ToBitsArray(input).ToList();
+
+            var numOfOnes = bits.Count(x => x);
+
+            var indexOfOnes = GetIndexesForOnes(bits);
+            
+            return new List<int>{numOfOnes}.Concat(indexOfOnes);
+        }
+
+        private IEnumerable<int> GetIndexesForOnes(IReadOnlyList<bool> bits)
+        {
+            for (var i = 0; i < bits.Count(); i++)
+            {
+                if(bits[i])
+                    yield return i;
+            }
+        }
+
+        private static IEnumerable<bool> ToBitsArray(int number)
+        {
+            var b = new BitArray(new[] {number});
+            var bits = new bool[b.Count];
+            b.CopyTo(bits, 0);
+            return bits;
         }
     }
 }
